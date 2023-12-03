1a1e721dee978f69fca133b1d3c3012dd9379c55
mdsohebl <123400053+mdsohebl@users.noreply.github.com>
2023-12-03 10:24:38 +0530

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,28 @@
+
+# path="emails.txt"
+# f=open(path,encoding="utf-8-sig")
+# data=f.read()
+# print(data)
+
+
+# info = data.strip().split("; ")
+
+# names = []
+# surnames = []
+# companies = []
+# emails = []
+
+# for i in info:
+#     name, email = i.split(" <")
+#     surname,first_name = name.split(", ")
+#     names.append(first_name)
+#     surnames.append(surname)
+#     email_parts = email.split("@")
+#     companies.append(email_parts[1])
+#     emails.append(email_parts[0] + "@" + email_parts[1])
+
+# print("Names:", names)
+# print("Surnames:", surnames)
+# print("Companies:", companies)
+# print("Email IDs:", emails)
+print("hello")
